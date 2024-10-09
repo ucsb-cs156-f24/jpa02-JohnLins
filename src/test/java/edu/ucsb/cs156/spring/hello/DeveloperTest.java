59bd5a45bb290b63f027c3334cfb1dea630e31c9
John Lins <johnpaullins@gmail.com>
2024-10-09 13:32:05 -0700

    Update everything, ready to deploy to Spring

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -27,7 +27,27 @@ public class DeveloperTest {
 
     @Test
     public void getGithubId_returns_correct_githubId() {
-        assertEquals("JohnLins", Developer.getGithubId);
+        assertEquals("JohnLins", Developer.getGithubId());
     }
+
+
+    @Test
+    public void getTeam_returns_team_with_correct_name() {
+        Team  t = Developer.getTeam();
+        assertEquals("f24-11", t.getName());
+    }
+
+    @Test
+    public void getTeam_returns_team_with_correct_members() {
+        Team  t = Developer.getTeam();
+        assertTrue(t.getMembers().contains("Aden"),"Team should contain Aden");
+        assertTrue(t.getMembers().contains("Brian"),"Team should contain Brian");
+        assertTrue(t.getMembers().contains("Frank"),"Team should contain Frank");
+        assertTrue(t.getMembers().contains("John"),"Team should contain John");
+        assertTrue(t.getMembers().contains("Samuel"),"Team should contain Samuel");
+        assertTrue(t.getMembers().contains("Jonathan"),"Team should contain Jonathan");
+    }
+
+
 
 }
